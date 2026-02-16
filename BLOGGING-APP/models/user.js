6ff2227b12754b507a910d8eb5ec59a1619671ac
Khushi Singh <khushiisingh100@gmail.com>
2026-02-16 22:04:09 +0530

    new update

---
+++ BLOGGING-APP/models/user.js
@@ -0,0 +1,26 @@
+const {Schema} = require('mongoose');
+
+const userSchema = new Schema({
+    fullName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    salt: { 
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    profileImageURL: {
+        type: String,
+    },
+},
+    {timestamps: true}
+);
