5217246b86d37f2bc574461b9847ce896c2a6cfa
Khushi Singh <khushiisingh100@gmail.com>
2026-02-17 08:01:57 +0530

    blog application completed

--- BLOGGING-APP/models/user.js
+++ BLOGGING-APP/models/user.js
@@ -1,4 +1,6 @@
-const {Schema} = require('mongoose');
+const {createHmac, randomBytes} = require('crypto');
+
+const {Schema, model} = require('mongoose');
 
 const userSchema = new Schema(
     {
@@ -12,7 +14,6 @@ const userSchema = new Schema(
         },
         salt: { 
             type: String,
-            required: true,
         },
         password: {
             type: String,
@@ -30,3 +31,38 @@ const userSchema = new Schema(
       },
       { timestamps: true }
 );
+
+userSchema.pre("save", async function (){
+    const user = this;
+    
+    if(!user.isModified("password")) return;
+
+    const salt = 'randomBytes(16).toString()';   //salt is a random string
+    const hashedPassword = createHmac("sha256", salt)
+       .update(user.password)
+       .digest("hex");
+
+    this.salt = salt;
+    this.password = hashedPassword;
+});
+
+userSchema.static('matchPassword', async function(email,password) {
+    const user = await this.findOne({email});
+    if(!user) throw new Error('User not found!');
+
+    const salt = user.salt;
+    const hashedPassword = user.password;
+
+    const userProvidedHash = createHmac("sha256", salt)
+     .update(password)
+     .digest("hex");
+
+     if(hashedPassword !== userProvidedHash)
+        throw new Error("Incorrect Password");
+
+     return user;
+})
+
+const User = model('user', userSchema);
+
+module.exports= User;
